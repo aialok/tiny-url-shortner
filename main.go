79f3232e595416b32ffbf7c3a088120e121c3f6f
Alok Gupta <alokgupta1560@gmail.com>
2025-12-26 23:11:15 +0530

    feat: Add HTTP handlers for URL shortening, retrieval, and redirection, and implement visit tracking for retrieved URLs.

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"strings"
 	"time"
 )
 
@@ -44,15 +47,111 @@ func SaveInDB(OriginalUrl string) {
 // Get URL from DB
 func GetFromDB(shortUrl string) (URL, bool) {
 	url, exists := db[shortUrl]
+	fmt.Println("Before:", url.Visits)
 	if !exists {
 		return URL{}, false
 	}
 	url.Visits++
+	db[shortUrl] = url
+	fmt.Println("After:", url.Visits)
 	return url, true
+}
+
+type HealthResponse struct {
+	Status string `json:"status"`
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	response := HealthResponse{
+		Status: "ok",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func urlHandler(w http.ResponseWriter, r *http.Request) {
+	shortURL := r.URL.Query().Get("short_url")
+	if shortURL == "" {
+		http.Error(w, "short_url is required", http.StatusBadRequest)
+		return
+	}
+	url, exists := GetFromDB(shortURL)
+	if !exists {
+		http.Error(w, "url not found", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(url); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func shortenHandler(w http.ResponseWriter, r *http.Request) {
+	originalURL := r.URL.Query().Get("original_url")
+	if originalURL == "" {
+		http.Error(w, "original_url is required", http.StatusBadRequest)
+		return
+	}
+	SaveInDB(originalURL)
+	url, exists := GetFromDB(GenerateShortUrl(originalURL))
+	if !exists {
+		http.Error(w, "url not found", http.StatusNotFound)
+		return
+	}
+	urlJSON := url
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(urlJSON); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+
+	if !strings.HasPrefix(path, "/r/") {
+		http.NotFound(w, r)
+		return
+	}
+
+	shortID := strings.TrimPrefix(path, "/r/")
+	if shortID == "" {
+		http.NotFound(w, r)
+		return
+	}
+
+	url, exists := GetFromDB(shortID)
+	if !exists {
+		http.NotFound(w, r)
+		return
+	}
+
+	http.Redirect(w, r, url.OriginalUrl, http.StatusFound)
 }
 
 func main() {
 	url := "https://www.google.com"
 	SaveInDB(url)
 	fmt.Println(GetFromDB(GenerateShortUrl(url)))
+
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/url", urlHandler)
+	http.HandleFunc("/shorten", shortenHandler)
+	http.HandleFunc("/r/", redirectHandler)
+	// Start a server
+	fmt.Println("Server running at port http://localhost:3000")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
